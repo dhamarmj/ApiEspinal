bcec6478c9a7e39c85a8b63a5681ac343204a149
Dhamar Martínez <39349434+dhamarmj@users.noreply.github.com>
2020-10-30 11:49:34 -0400

    url again

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace ApiEspinal
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSetting("https_port", "5001");
                     webBuilder.UseStartup<Startup>();
                 });
     }
